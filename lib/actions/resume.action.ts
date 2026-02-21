6a4e61d251d9a54869183d8acfbb41c87dd4e194
Vikrant Singh <vikrantsinghan5@gmail.com>
2026-02-21 18:16:41 +0530

    dfghdh

--- lib/actions/resume.action.ts
+++ lib/actions/resume.action.ts
@@ -97,6 +97,7 @@ export async function createResumeAnalysis(
         categoryScoresObj.formatting = category.score;
       }
     });
+    
 
      // If some categories weren't found, use averages
     const scores = Object.values(categoryScoresObj).filter(s => s > 0);
